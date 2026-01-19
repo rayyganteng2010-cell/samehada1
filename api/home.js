8ea801f9e75dc393ef24b172ecea3671caca2e80
rayyganteng2010-cell <rayyganteng2010@gmail.com>
2026-01-20 06:24:46 +0700

    Create home.js

---
+++ api/home.js
@@ -0,0 +1,12 @@
+const { getHomePage } = require('../lib/scraper');
+
+module.exports = async (req, res) => {
+  try {
+    const data = await getHomePage();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
