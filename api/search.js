8813ff768ef7e6666e0dd17706efe7d41433a7ce
rayyganteng2010-cell <rayyganteng2010@gmail.com>
2026-01-20 06:25:05 +0700

    Create search.js

---
+++ api/search.js
@@ -0,0 +1,18 @@
+const { searchAnime } = require('../lib/scraper');
+
+module.exports = async (req, res) => {
+  try {
+    const { q } = req.query;
+    
+    if (!q) {
+      return res.status(400).json({ error: 'Query parameter "q" is required' });
+    }
+    
+    const data = await searchAnime(q);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
